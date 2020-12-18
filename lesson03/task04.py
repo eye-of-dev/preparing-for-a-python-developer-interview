824a0399df621396d22d53d5ca36c526d97e7bae
Anton Lisikov <anton.lisikov@gmail.com>
2020-12-18 16:40:08 +0300

    Урок 3. Python - стандартная библиотека Python.

--- lesson03/task04.py
+++ lesson03/task04.py
@@ -13,17 +13,70 @@
     построчный вывод содержимого. Вся программа должна запускаться по
     вызову первой функции.
 """
+import os
+import random
 
 
-def prepare_file():
+def get_string():
+    """
+    Генерация случайной строки
+    :return: Случайная строка
+    """
+    return ''.join([chr(random.randint(97, 122)) for _ in range(11)])
 
-    spam = 'text.txt'
-    text_list = [chr(i) for i in range(97, 123)]
 
-    print(chr(91))
-    print(ord('A'))
-    print(ord('Z'))
+def read_file(file_path):
+    """
+    Построчный вывод из файла в консоле
+    :param file_path: Ссылка на файл
+    :return: None
+    """
+    with open(file_path) as fn:
+        for line in fn:
+            print(line)
 
-    # pass
 
-prepare_file()
+def prepare_file(text_list, numb_list):
+    """
+    Запись в файл комбинации строка - число
+    :param text_list: Список случайних строк
+    :param numb_list: Список случайных чисел
+    :return: function
+    """
+    file_path = f'{os.path.abspath(os.getcwd())}/text.txt'
+
+    if not os.path.exists(file_path):
+        print('Файла не существует. Он будет создан')
+
+    with open(file_path, 'w+') as fn:
+        for text, numb in zip(text_list, numb_list):
+            fn.write(f'{text} : {numb} \n')
+
+    return read_file(file_path)
+
+
+TEXT_LIST = [get_string() for _ in range(11)]
+NUMB_LIST = [random.randint(10000, 99999) for _ in range(11)]
+
+prepare_file(TEXT_LIST, NUMB_LIST)
+
+# Результат
+# euvarlarshd : 12475
+# onhmedbpkui : 84253
+# lugxvrsvmuo : 12719
+# kkgcbokunga : 87649
+# eudwyieiebz : 56143
+# adptfqurccs : 24812
+# valuhzxeftf : 33322
+# wnvdrbywrva : 20193
+# bvmlyzlokbf : 43327
+# hnyafruslih : 64517
+# fqafqrsxsvx : 26926
+
+# Результат pylint
+# ************* Module task04
+# task04.py:34:28: C0103: Variable name "fn" doesn't conform to snake_case naming style (invalid-name)
+# task04.py:51:34: C0103: Variable name "fn" doesn't conform to snake_case naming style (invalid-name)
+#
+# ------------------------------------------------------------------
+# Your code has been rated at 8.95/10
